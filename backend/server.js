8b2b541ea20bf5c069ba8bfd7f94934df516a128
ShubhashishSingh <shubhahshish@gmail.com>
2026-05-24 22:32:36 +0530

    Fix uploaded CV static path

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
     res.send("Immigration Backend Running Successfully");
