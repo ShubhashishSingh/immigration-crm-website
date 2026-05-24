954fb95132264091b77e5b74eae40bce608cfada
ShubhashishSingh <shubhahshish@gmail.com>
2026-05-25 00:13:27 +0530

    Use Railway variables for Google Sheets

--- backend/googleSheets.js
+++ backend/googleSheets.js
@@ -1,7 +1,21 @@
 const { google } = require("googleapis");
 
+const credentials = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com"
+};
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: "google-sheet-key.json",
+  credentials,
   scopes: ["https://www.googleapis.com/auth/spreadsheets"]
 });
 
@@ -14,7 +28,7 @@ async function addLeadToSheet(data) {
   });
 
   await sheets.spreadsheets.values.append({
-    spreadsheetId: "1lqRLc-oeQ4fs_lPqEXmOicjg8jxc76BR60p4ry4K5mk",
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
     range: "Sheet1!A:I",
     valueInputOption: "USER_ENTERED",
     resource: {
